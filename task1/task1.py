8f29f8d4cf9647b3dc150470a3f69705e14960f3
Ivan Arkhipov <vanchez666@gmail.com>
2024-12-21 14:14:11 +0300

    refactored task1

--- task1/task1.py
+++ task1/task1.py
@@ -1,21 +1,21 @@
+def normalize_step(step: int) -> int:
+    if step > 0:
+        return step - 1
+    if step < 0:
+        return step + 1
+    return 0
+
+
 def main() -> None:
-    num_digits, step_length = map(int, input().split())
+    num_digits, step = map(int, input().split())
 
-    # if step_length <= 0:
-    #     print('The step length must be a positive number.')
-    #     return
-
-    if num_digits == 1:
-        print(1)
-        return
-
-    digits = list(range(1, num_digits + 1))
-    path = str(digits[0])
-    current_index = step_length % num_digits - 1
+    step_normalized = normalize_step(step)
+    current_index = step_normalized % num_digits
+    path = '1'
 
     while current_index != 0:
-        path += str(digits[current_index])
-        current_index = (current_index + step_length) % num_digits - 1
+        path += str(current_index + 1)
+        current_index = (current_index + step_normalized) % num_digits
 
     print(path)
 
